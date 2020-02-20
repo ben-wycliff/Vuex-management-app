c909eae33474abac1556d0d7983ba1b9d5388c22
ben-wycliff <ben12wycliff@gmail.com>
2020-02-20 18:04:48 +0300

    Creates addTodo action and newTodo mutation

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -24,11 +24,17 @@ const actions = {
         const res = await Axios.get('https://jsonplaceholder.typicode.com/todos');
         console.log(res.data);
         commit('setTodos', res.data)
+     },
+     async addTodo( { commit }, title){
+         const res = await Axios.post('https://jsonplaceholder.typicode.com/todos', {title, completed: false});
+         commit('newTodo', res.data)
+
      }
 };
 
 const mutations = {
-    setTodos: (state, todos) => (state.todos=todos)
+    setTodos: (state, todos) => (state.todos=todos),
+    newTodo: (state, todo) => state.todos.unshift(todo)
 };
 
 export default {
