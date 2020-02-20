35a61c789368b72b0150174a4cbb6d2fd868b249
ben-wycliff <ben12wycliff@gmail.com>
2020-02-20 18:04:06 +0300

    Adds AddTodo component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo />
       <Todos />
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
-
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    AddTodo
   }
 }
 </script>
